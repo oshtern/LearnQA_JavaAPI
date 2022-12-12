e1d2e85536c784b4e8ea53ad2f17813289f70e76
oshtern <olesya0789@gmail.com>
2022-12-12 10:27:01 +0700

    Ex7

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -14,17 +14,31 @@ public class HelloWorldTest {
 //        headers.put("myHeader1", "myValue1");
 //        headers.put("myHeader2", "myValue2");
 
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
-                .andReturn();
+        String resource = "https://playground.learnqa.ru/api/long_redirect";
+        Response code = RestAssured.get(resource);
+        int counter = 0;
 
-        response.prettyPrint();
-        String locationHeader = response.getHeader("Location");
-        System.out.println(locationHeader);
+        for (int i = 0; i < 50; i++) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(true)
+                    .when()
+                    .get(resource)
+                    .andReturn();
+
+            counter = counter + 1;
+//            response.prettyPrint();
+            String locationHeader = response.getHeader("Location");
+            System.out.println(locationHeader);
+            resource = locationHeader;
+            int statusCode = code.getStatusCode();
+            System.out.println(statusCode);
+            System.out.println(counter);
+
+        if (statusCode == 200) {
+            break;
+        }
+        }
     }
-
 }
