8a72898e6a5b016d89210d581921225cc8d4d977
oshtern <olesya0789@gmail.com>
2023-01-09 16:59:17 +0700

    Ex13 fixes

--- src/test/java/Ex13.java
+++ src/test/java/Ex13.java
@@ -28,13 +28,13 @@ public class Ex13 {
 //            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
 //            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
 
-    @CsvSource({"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30! Mobile! No! Android",
-            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1! Mobile! Chrome! iOS",
-            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)! Googlebot! Unknown! Unknown",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0! Web! Chrome! No",
-            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1! Mobile! No! iPhone"})
+    @ValueSource(strings = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30!Mobile!No!Android",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1!Mobile!Chrome!iOS",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)!Googlebot!Unknown!Unknown",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0!Web!Chrome!No",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1!Mobile!No!iPhone"})
     public void receiveInfoOnUser(String user_agent) {
-        String[] expectedValues = user_agent.split("! ");
+        String[] expectedValues = user_agent.split("!");
         Map<String, String> headers = new HashMap<>();
 
         headers.put("user-agent", user_agent);
@@ -54,7 +54,7 @@ public class Ex13 {
         String device = response.get("device");
         System.out.println(device);
 
-        System.out.println(expectedValues[0]);
+        System.out.println(expectedValues[2]);
 
 //        assertEquals(expectedValues, platform, "Unexpected platform. Actual is " + platform);
 
